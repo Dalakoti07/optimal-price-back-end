d0c0137f217eeca312dd3ff439ffc4f948c669e0
lonewolf <saurabhdalakoti07@gmail.com>
2020-08-23 17:55:19 +0530

    permissions are added to ensure protected apis

--- authApp/urls.py
+++ authApp/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from .views import UserViewSet
+from rest_framework_jwt.views import obtain_jwt_token
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^login/', include('rest_auth.urls')),
+    url(r'^login/', obtain_jwt_token),
 ]
--- authApp/views.py
+++ authApp/views.py
@@ -6,7 +6,24 @@ from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
 
+# Also add these imports
+from .permissions import IsLoggedInUserOrAdmin, IsAdminUser
+from rest_framework.permissions import AllowAny
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    # Add this code block
+    def get_permissions(self):
+        permission_classes = []
+        # anyone can create account
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        elif self.action == 'retrieve' or self.action == 'update' or self.action == 'partial_update':
+            # a user can see his details, and update his own details, or either user can do this
+            permission_classes = [IsLoggedInUserOrAdmin]
+        elif self.action == 'list' or self.action == 'destroy':
+            # only admin can view all users and delete a user
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]

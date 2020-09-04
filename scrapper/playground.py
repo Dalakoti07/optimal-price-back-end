e5240f712a6d818b54f56dccc3c3f38acede172e
lonewolf <saurabhdalakoti07@gmail.com>
2020-09-04 12:15:48 +0530

    switched to python 3.8

--- scrapper/playground.py
+++ scrapper/playground.py
@@ -25,9 +25,27 @@ print(string.split('(')[0])
 
 '''
 
+'''
 import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--item", help="enter the item name you want to search on flipkart",type=str)
 args = parser.parse_args()
 print("keyword searched :"+args.item)
+'''
+import asyncio
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+
+if __name__ == "__main__":
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
